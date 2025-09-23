0734f7cabce55aa40ca06fbe6da3f681a4070803
Ramin Toosi <ramin.tsi@gmail.com>
2025-09-23 08:26:37 +0330

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn find_pattern(pattern: &str) -> (bool, String) {
         if special_chars.contains(&c) {
             if current_pattern.len() > 0{
                 // it means we already have a patter so we should not open a new special pattern
-                break;    
+                if c != '+' {break;}    
             }
             else {
                 current_pattern.push(c);
@@ -24,6 +24,21 @@ fn find_pattern(pattern: &str) -> (bool, String) {
             current_pattern.push(c);
             if !flag_special_char {
                 flag_special_char = true;
+            }
+        }
+        else if c == '+' {
+            if current_pattern.len() == 1 {
+                current_pattern.push(c);
+                flag_special_char = true;
+                break;
+            }
+            else if current_pattern.len() > 1 {
+                current_pattern.pop();
+                break;
+            }
+            else {
+                // raise errot we do not know what to do
+                panic!("Invalid pattern: {}", pattern);
             }
         }
         else {
@@ -51,6 +66,7 @@ fn match_special_pattern(input_line: &str, pattern: &str) -> (bool, usize){
         else if pattern == r"\w" && (c.is_alphanumeric() || c == '_') {return (true, index);}
         else if pattern.starts_with("[^") {if !pattern[1..pattern.len()-1].contains(c) {return (true, index);}}
         else if pattern.starts_with("[") {if pattern[1..pattern.len()-1].contains(c) {return (true, index);}}
+        else if pattern.ends_with("+") && input_line.contains(c) {return (true, index);}
     }
     return (false, 0);
 
